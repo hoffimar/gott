003537f5a7d34768f6d826eb15b7973a770be592
Martin Hoffmann <hoffimar@gmail.com>
2021-04-12 21:15:41 +0200

    Added command 'edit' to edit storage file in configurable editor

--- cmd/root.go
+++ cmd/root.go
@@ -58,6 +58,7 @@ func initConfig() {
 		viper.SetConfigName(".gott.yaml")
 		viper.SetConfigType("yaml")
 		viper.SetDefault("StorageLocation", path.Join(home, "/.gott"))
+		viper.SetDefault("Editor", "vim")
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
